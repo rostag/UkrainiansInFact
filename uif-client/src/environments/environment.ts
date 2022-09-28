f14a44b9eee267dff771153e2fc67a3bed0d0790
Rost Siryk <rostislav.siryk@gmail.com>
2022-09-28 22:24:01 +0300

    uif - initial tasks module

---
+++ uif-client/src/environments/environment.ts
@@ -0,0 +1,25 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyB9qso1TvQJwVv_7uGDHrsm7CTyw9XmWV0",
+    authDomain: "arekuni.firebaseapp.com",
+    projectId: "arekuni",
+    storageBucket: "arekuni.appspot.com",
+    messagingSenderId: "50131028447",
+    appId: "1:50131028447:web:56c97bd406036bd09b1c6a",
+    measurementId: "G-JS38WZ0R3M"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
