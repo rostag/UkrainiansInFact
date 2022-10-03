ec8c96a240cdde9ade9dfc69b56031818a8f946d
Rost Siryk <rostislav.siryk@gmail.com>
2022-10-03 15:23:30 +0300

    reverse landing logic and add news page

--- uif-client/src/app/modules/main/main.module.ts
+++ uif-client/src/app/modules/main/main.module.ts
@@ -5,13 +5,15 @@ import { MainRoutingModule } from './main-routing.module';
 import { MainComponent } from './components/main/main.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { WhatsNextComponent } from './components/whats-next/whats-next.component';
+import { HomeComponent } from './components/home/home.component';
 
 
 @NgModule({
   declarations: [
     MainComponent,
     DashboardComponent,
-    WhatsNextComponent
+    WhatsNextComponent,
+    HomeComponent
   ],
   imports: [
     CommonModule,
