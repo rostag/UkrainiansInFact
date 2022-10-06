e00a9ff8e8d5e23f6dbb90125a203fd323ec0129
Rost Siryk <rostislav.siryk@gmail.com>
2022-10-06 23:23:58 +0300

    streamline text style

--- uif-client/functions/src/index.ts
+++ uif-client/functions/src/index.ts
@@ -1,9 +1,18 @@
 import * as functions from "firebase-functions";
+import * as admin from "firebase-admin";
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-export const helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!");
+admin.initializeApp();
+
+export const addMessage = functions.https.onRequest(async (req, res) => {
+  const orig = req.query.text;
+  const writeResult = await admin.firestore().collection('messages').add({original: orig});
+  res.json({result: 'Message with ID: ' + writeResult.id + 'added'});
 });
+
+export const makeUppercase = functions.firestore.document('/messages/{documentId')
+.onCreate((snap, context) => {
+  const orig = snap.data().original;
+  functions.logger.log('Uppercasing', context.params.documentId, orig);
+  const uppercase = orig.toUppperCase();
+  return snap.ref.set({uppercase}, {merge: true});
+})
