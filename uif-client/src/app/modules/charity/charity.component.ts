7e7af1a851d4ea45dfc1bd1604add3f393500379
Rost Siryk <rostislav.siryk@gmail.com>
2022-11-27 19:50:23 +0200

    feat(charity): download teaser image

--- uif-client/src/app/modules/charity/charity.component.ts
+++ uif-client/src/app/modules/charity/charity.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import html2canvas from 'html2canvas';
 
 @Component({
   selector: 'app-charity',
@@ -7,13 +8,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CharityComponent implements OnInit {
 
+  imgData!: string;
+  imgDownload!: string;
+  downloadReady = false;
+
   data = {
     gathered: 23000
   }
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  copyImage() {
+    const imageElement = document.getElementById('image-teaser') as HTMLElement;
+    html2canvas(imageElement).then(canvas => {
+      this.saveImgFromCanvas(canvas);
+    });
+    
+  }
+
+  saveImgFromCanvas(c: HTMLCanvasElement) {
+    var imageData = c.toDataURL("image/png");
+    // Now browser starts downloading it instead of just showing it
+    this.imgData = imageData.replace(/^data:image\/png/, "data:application/octet-stream");
+    this.imgDownload = "image.png";
+    this.downloadReady = true;
+  }
+
+  invalidate() {
+    this.downloadReady = false;
   }
 
 }
